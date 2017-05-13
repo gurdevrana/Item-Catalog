eeda07745764113b76b9b5c8e6f63bc354e6dde5
gurdevrana <gurdev.rana@gmail.com>
2017-05-13 21:46:56 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,28 +1,31 @@
-import sqlite3
+import psycopg2
 
-conn = sqlite3.connect('catalog')
+conn = psycopg2.connect(database="postgres", user="postgres", password="babaji9lm", host="localhost", port="5432")
+conn.autocommit = True
 c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS categories
-(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-)
-""")
-conn.commit()
-c.execute("""CREATE TABLE IF NOT EXISTS item
-(
-    id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    description TEXT,
-    category TEXT NOT NULL,
-    user TEXT NOT NULL
-)
 
-""")
+c.execute("""CREATE DATABASE CATELOG; """)
 conn.commit()
-c.execute("""CREATE TABLE IF NOT EXISTS users
-(
-    username TEXT,
-    email TEXT PRIMARY KEY
+conn.close()
+
+conn = psycopg2.connect(database="catelog", user="postgres", password="babaji9lm", host="localhost", port="5432")
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS public.item2 (
+  id  SERIAL PRIMARY KEY  ,
+  name TEXT NOT NULL,
+  description TEXT,
+  category TEXT NOT NULL,
+  user_email TEXT NOT NULL
 )""")
+conn.commit()
+c.execute("""CREATE TABLE IF NOT EXISTS public.categories2 (
+  id  SERIAL  PRIMARY KEY NOT NULL,
+  name TEXT
+);""")
+conn.commit()
+c.execute("""CREATE TABLE IF NOT EXISTS users2 (
+  username TEXT,
+  email TEXT PRIMARY KEY NOT NULL
+);""")
 conn.commit()
